ac5851f40516798eaa28399c9da7a0dedc11559c
Joseph <joseph@titaniumcoast.com>
2022-02-02 14:36:00 +0000

    chore(root): fixed Commitlint subject capitalization

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,6 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "subject-case": [
-            2,
-            "always",
-            ["sentence-case", "start-case", "lower-case"],
-        ],
         "scope-enum": [2, "always", ["root", "core"]],
         "scope-empty": [2, "never"],
     },
