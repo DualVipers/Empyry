a44d93dd248bbeacf2977ca2dc18b6fbb410f48c
Joseph <joseph@titaniumcoast.com>
2022-10-31 01:22:52 +0000

    feat(core): added admin creation during init process
    The admin is only added when no Users exist, a.k.a. a new server.

--- core/src/migrate.js
+++ core/src/migrate.js
@@ -1,5 +1,18 @@
-import { runMigration, default as knex } from "./database.js";
+import { runMigration, User, default as knex } from "./database.js";
+import logger from "./logger.js";
+import { hash } from "./password.js";
 
 await runMigration();
+
+if ((await User.query().count()) < 1) {
+    logger.info(`Creating Admin User ${"admin"}`);
+
+    await User.query().insert({
+        username: "admin",
+        password_hash: await hash("admin"),
+        email: "admin@example.com",
+        admin: true,
+    });
+}
 
 await knex.destroy();
